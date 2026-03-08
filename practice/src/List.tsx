0f9ccf3471261c5a1b4dcf5ccfe2a1776f10c88f
Han Kwan Saing <saihankwansaing@gmail.com>
2026-03-08 18:35:43 +0900

    practice: rendering list and use sort, filter, map with the list

--- practice/src/List.tsx
+++ practice/src/List.tsx
@@ -1,12 +1,22 @@
 function RanderingList() {
 
-    const fastFoods: string[] = ["hamburger", "pizza", "cup noodle", "fried chicken", "fried, rice"]
+    const fastFoods= [  {id: 1, name: "hamburger", price: 500}, 
+                        {id: 2, name: "pizza", price: 800}, 
+                        {id: 3, name: "cup noodle", price: 200}, 
+                        {id: 4, name: "fried chicken", price: 600}]
+
+    // fastFoods.sort((a, b) => a.name.localeCompare(b.name)) // alphabetically
+    // fastFoods.sort((a, b) => b.price - a.price) // high to low price
+    const expensivefastFoods = fastFoods.filter(fastFood => fastFood.price >= 500)
+    expensivefastFoods.sort((a, b) => b.price - a.price)
+
+    const listedFoods = expensivefastFoods.map(food => <li key={food.id}>
+                            {food.name}: ${food.price}
+                        </li>)   
 
     return (
         <ul>
-            {fastFoods.map(food => (
-                <li key={food}>{food}</li>
-            ))}
+            {listedFoods}
         </ul>
     )
 }
