a336e069074e2bcb1c56c18e43f4cc977c717255
Han Kwan Saing <saihankwansaing@gmail.com>
2026-03-09 22:26:10 +0900

    practice: made simple counter using useState

--- practice/src/component/Click.tsx
+++ practice/src/component/Click.tsx
@@ -1,24 +1,49 @@
+import { useState } from "react"
 import CatPic from "../assets/cat.jpg"
 
 function Click() {
 
-    let count = 0
-
+    // simple click event
+    let bur = 0
     const handleClick = (food: string) => {
         
-        if (count >= 3) {
+        if (bur >= 3) {
             console.log("I'm full ~.~")
         } else {
-            count++
-            console.log(`I ate ${count} ${food}/s`)
+            bur++
+            console.log(`I ate ${bur} ${food}/s`)
         }
+    }
 
+
+    // counter using useStat
+    const [count, setCount] = useState(0);
+    
+    const decBtn = () => {
+        setCount(count - 1)     
+    }
+    const resetBtn = () => {
+        setCount(0)
+    }
+    const incBtn = () => {
+        setCount(count + 1)
     }
 
     return(
-        <img src={CatPic} alt="cat-pic" className="click-cat"
-            onClick={() => handleClick("hamburger")}
-        />
+        <>
+            {/* for click event */}
+            <img src={CatPic} alt="cat-pic" className="click-cat"
+            onClick={() => handleClick("hamburger")}></img>
+
+            {/* for counter using useStat */}
+            <div className="counter">
+                <div>{count}</div>
+                <button onClick={decBtn}>Decrease</button>
+                <button onClick={resetBtn}>Reset</button>
+                <button onClick={incBtn}>Increase</button>
+            </div>
+        </>
+        
     )   
 }
 
