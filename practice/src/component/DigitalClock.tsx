85b7f10c98cdffa990546308fe19cb3845fb26e0
Han Kwan Saing <saihankwansaing@gmail.com>
2026-03-14 19:00:51 +0900

    feat: practiced useEffect with digital clock example

--- practice/src/component/DigitalClock.tsx
+++ practice/src/component/DigitalClock.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+
 function DigitalClock() {
     
+    const [time, setTime] = useState(new Date)
+
+    useEffect(() => {
+        const setIntervalId = setInterval(() => {
+            setTime(new Date)
+        }, 1000);
+
+        return () => {
+            clearInterval(setIntervalId)
+        }
+    })
+
+    const formattedTime = () => {
+        let hours = time.getHours()
+        const minutes = time.getMinutes().toString().padStart(2, "0")
+        const seconds = time.getSeconds().toString().padStart(2, "0")
+
+        const amOrPm = hours >= 12 ? "PM" : "AM"
+
+        hours % 12 || 12
+        const formattedHours = hours.toString().padStart(2, "0")
+
+        return `${formattedHours}:${minutes}:${seconds} ${amOrPm}`
+    }
 
     return (
         <div className="digital-clock">
-            <span>00:00:00 pm</span>
+            <span>{formattedTime()}</span>
         </div>
     )
 }
