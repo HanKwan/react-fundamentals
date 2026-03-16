64d2fac6d824b21bea4969adb272cfc670c39fea
Han Kwan Saing <saihankwansaing@gmail.com>
2026-03-16 22:45:56 +0900

    update: add stop and reset btn

--- practice/src/component/Timer.tsx
+++ practice/src/component/Timer.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react"
+
 function Timer() {
     
+    const [isRunning, setIsRunning] = useState(false)
+    const [endTime, setEndTime] = useState(0)
+    
+    const handleStart = () => {
+        
+    } 
 
 
     return(
         <div className="timer">
             <div className="timer-layout">   
                 <div className="input-time">
-                <input type="number" />
-                <button>Start</button>
+                    <input type="number" />
+                    <button className="startBtn">Start</button>
+                    <button className="stopBtn">Stop</button>
+                    <button className="resetBtn">Reset</button>
                 </div>
                 <div className="timer-display">00:00</div>
             </div>
