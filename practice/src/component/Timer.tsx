af82c6a0aef4ffeaecbafc6dea87b1ebb79cb3ad
Han Kwan Saing <saihankwansaing@gmail.com>
2026-03-17 14:56:56 +0900

    refactor handleStart and add useEffect

--- practice/src/component/Timer.tsx
+++ practice/src/component/Timer.tsx
@@ -1,14 +1,31 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 function Timer() {
     
     const [isRunning, setIsRunning] = useState(false)
     const [remainingTime, setRemainingTime] = useState(0)
-    const endTimeRef = useRef(null);
-    const intervalIdRef = useRef(null)
+    const endTimeRef = useRef<number | null>(null);
+    const intervalIdRef = useRef<number | null>(null)
+
+    useEffect(() => {
+            intervalIdRef.current = setTimeout(() => {
+                if (endTimeRef.current) {
+                    setRemainingTime(endTimeRef.current - Date.now())
+                }
+            }, 1000);
+
+            return () => {
+                if (intervalIdRef.current) {
+                    clearInterval(intervalIdRef.current)
+                }
+            }
+
+    }, [isRunning])
 
     const handleStart = () => {
-        
+        const duration = remainingTime * 1000 * 60
+        endTimeRef.current = Date.now() + duration
+        setIsRunning(true)
     }
     const handleStop = () => {
         setIsRunning(false)
@@ -24,10 +41,10 @@ function Timer() {
         <div className="timer">
             <div className="timer-layout">   
                 <div className="input-time">
-                    <input type="number" />
-                    <button className="startBtn">Start</button>
-                    <button className="stopBtn">Stop</button>
-                    <button className="resetBtn">Reset</button>
+                    <input type="number" onChange={(e) => setRemainingTime(Number(e.target.value))}/>
+                    <button className="startBtn" onClick={handleStart}>Start</button>
+                    <button className="stopBtn" onClick={handleStop}>Stop</button>
+                    <button className="resetBtn" onClick={handleReset}>Reset</button>
                 </div>
                 <div className="timer-display">00:00</div>
             </div>
